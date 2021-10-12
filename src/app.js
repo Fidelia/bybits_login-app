cc8a1b8314758a3aa3c4fb7b24c3da4b3098f58e
Fidelia <fidelia_okandze@hotmail.co.uk>
2021-10-13 00:15:40 +0100

    axios post request

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-var userName = "test";
-var password = "test";
-
-function login(form) {
-  if (form.username.value == "test" && form.password.value == "test") {
-    self.location.href = "policy.html";
-  } else {
-    alert("Wrong username or password");
-    return false;
-  }
+function getUserAccount() {
+  return axios.get("https://api.bybits.co.uk/auth/token");
+  console.log(response);
 }
+
+function getUserPermissions() {
+  return axios.get("https://api.bybits.co.uk/policys/details");
+}
+
+Promise.all([getUserAccount(), getUserPermissions()]).then(function (results) {
+  const acct = results[0];
+  const perm = results[1];
+});
